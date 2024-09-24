c3d7f7d025aa5ac275cf974a928b785696b32153
Moin-hub <01766moinmlpmlp@gmail.com>
2024-09-24 15:06:27 +0600

    new commit

--- script.js
+++ script.js
@@ -1,187 +1,83 @@
-
 function takeInnerText(id){
     const getEl = document.getElementById(id).innerText;
     const getElNumber = parseFloat(getEl);
     return getElNumber;
 };
-
-function getInputValue(id){
+function getInputValue(id) {
     const getVL = document.getElementById(id).value;
     const getVLNumber = parseFloat(getVL);
     return getVLNumber;
+};
+
+document.getElementById("btn-donation").addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        window.location.href='index.html';
+})
+document.getElementById("btn-history").addEventListener('click', function(event) {
+        event.preventDefault();
+        window.location.href='history.html';
+})
+
+////////////////////////////////////////////////////////////////////////
+// function showSectionById(id) {
+//     document.getElementById('homePage').classList.add('hidden');
+//     document.getElementById('historyPage').classList.add('hidden');
+
+//     // // show the section with the provided id as parameter
+//     document.getElementById(id).classList.remove('hidden');
+// }
+// document.getElementById("btn-history").addEventListener('click', function(event) {
+    //     event.preventDefault();
+//     showSectionById('historyPage');
+// })
+// document.getElementById("btn-donation").addEventListener('click', function(event) {
+    //     event.preventDefault();
+//     showSectionById('homePage');
+// })
+///////////////////////////////////////////////////////////////////////////
+function handleDonation(donateBtnId, inputId, totalBalanceId, semiTotalId){
+    document.getElementById(donateBtnId).addEventListener('click', function(event) {
+        event.preventDefault();
+        const inputNumber = getInputValue(inputId);
+        if(inputNumber.length === 0) {
+            alert("Please Enter Your Amount!!");
+            return;
+        }
+        else if(isNaN(inputNumber) === true) {
+            alert("Invalid amount value!!!");
+            return;
+        }
+        else if(inputNumber < 0) {
+            alert("Invalid amount value!!!");
+            return;
+        }
+        else if(inputNumber === 0) {
+            alert("Amount is 0 BDT");
+        }
+        else if(inputNumber > 0) {
+            const totalBalanceNumber = takeInnerText(totalBalanceId);
+            const semiTotalNumber = takeInnerText(semiTotalId);
+
+            let newTotalBalance = totalBalanceNumber - inputNumber;
+            let newSemiTotal = semiTotalNumber + inputNumber;
+
+            if(newTotalBalance < 0) {
+                alert("Insufficient Balance!!");
+                return;
+            }
+
+            document.getElementById(totalBalanceId).innerText = newTotalBalance;
+            document.getElementById(semiTotalId).innerText = newSemiTotal;
+            document.getElementById(inputId).value = '';
+
+            my_modal_5.showModal();
+
+        
+        }
+    });
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    document.getElementById("donateNowOne").addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    const inputNumber = getInputValue('inputOne');
-    if(inputNumber.length === 0) {
-        alert("Please Enter Your Amount!!");
-        return;
-    }
-    else if(isNaN(inputNumber)===true){
-        alert("Invalid Amount Value!!!");
-        return;
-    }
-    else if(inputNumber < 0){
-        alert("Invalid Amount Value!!!");
-        return;
-    }
-    else if(inputNumber===0){
-        alert("Amount is 0BDT!!!");
-        return;
-    }
-    else if(inputNumber > 0) {       
-        const totalBalanceNumber = takeInnerText('totalBalance');
-        const semiTotalNumber = takeInnerText('firstTotal');
-
-        let newTotalBalance = totalBalanceNumber - inputNumber;
-        let newSemiTotal = semiTotalNumber + inputNumber;
-
-        if(newTotalBalance < 0) {
-            alert("Insufficient Balance!!!")
-            return;
-        }
-         document.getElementById('totalBalance').innerText = newTotalBalance;
-         document.getElementById("firstTotal").innerText = newSemiTotal;
-         document.getElementById('inputOne').value = '';  
-    
-        my_modal_5.showModal();
-
-    }   
-});
-/////////////////////////////////////////////////////////
-    document.getElementById("donateNowTwo").addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    const inputNumber = getInputValue('inputTwo');
-    if(inputNumber.length === 0) {
-        alert("Please Enter Your Amount!!");
-        return;
-    }
-    else if(isNaN(inputNumber)===true){
-        alert("Invalid Amount Value!!!");
-        return;
-    }
-    else if(inputNumber < 0){
-        alert("Invalid Amount Value!!!");
-        return;
-    }
-    else if(inputNumber===0){
-        alert("Amount is 0BDT!!!");
-        return;
-    }
-    else if(inputNumber > 0) {       
-        const totalBalanceNumber = takeInnerText('totalBalance');
-        const semiTotalNumber = takeInnerText('secondTotal');
-
-        let newTotalBalance = totalBalanceNumber - inputNumber;
-        let newSemiTotal = semiTotalNumber + inputNumber;
-
-        if(newTotalBalance < 0) {
-            alert("Insufficient Balance!!!")
-            return;
-        }
-         document.getElementById('totalBalance').innerText = newTotalBalance;
-         document.getElementById("secondTotal").innerText = newSemiTotal;
-         document.getElementById('inputTwo').value = '';  
-                
-        my_modal_5.showModal();
-    
-    }   
-});
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    document.getElementById("donateNowThree").addEventListener('click', function(event) {
-    event.preventDefault();
-    
-
-        // validation(inputThree, thirdTotal, totalBalance);
-        
-
-    const inputNumber = getInputValue('inputThree');
-    if(inputNumber.length === 0) {
-        alert("Please Enter Your Amount!!");
-        document.getElementById('inputThree').value = ''; 
-        return;
-    }
-    else if(isNaN(inputNumber)===true){
-        alert("Invalid Amount Value!!!");
-        document.getElementById('inputThree').value = ''; 
-        return;
-    }
-    else if(inputNumber < 0){
-        alert("Invalid Amount Value!!!");
-        document.getElementById('inputThree').value = ''; 
-        return;
-    }
-    else if(inputNumber===0){
-        alert("Amount is 0BDT!!!");
-        document.getElementById('inputThree').value = ''; 
-        return;
-    }
-    else if(inputNumber > 0) {       
-        const totalBalanceNumber = takeInnerText('totalBalance');
-        const semiTotalNumber = takeInnerText('thirdTotal');
-
-        let newTotalBalance = totalBalanceNumber - inputNumber;
-        let newSemiTotal = semiTotalNumber + inputNumber;
-
-        if(newTotalBalance < 0) {
-            alert("Insufficient Balance!!!")
-            return;
-        }      
-         document.getElementById('totalBalance').innerText = newTotalBalance;
-         document.getElementById("thirdTotal").innerText = newSemiTotal;
-         document.getElementById('inputThree').value = ''; 
-
-         my_modal_5.showModal();   
-
-
-    }   
-});
-
-/////////////////////////////////////////////////////////////////////////////////
-// function validation(inputID, semiTotal, finalTotal){
-//     const inputNumber = getInputValue(inputID);
-//     if(inputNumber.length === 0) {
-//         alert("Please Enter Your Amount!!");
-//         document.getElementById(inputID).value = ''; 
-//         return;
-//     }
-//     else if(isNaN(inputNumber)===true){
-//         alert("Invalid Amount Value!!!");
-//         document.getElementById(inputID).value = ''; 
-//         return;
-//     }
-//     else if(inputNumber < 0){
-//         alert("Invalid Amount Value!!!");
-//         document.getElementById(inputID).value = ''; 
-//         return;
-//     }
-//     else if(inputNumber===0){
-//         alert("Amount is 0BDT!!!");
-//         document.getElementById(inputID).value = ''; 
-//         return;
-//     }
-//     else if(inputNumber > 0) {       
-//         const totalBalanceNumber = takeInnerText(finalTotal);
-//         const semiTotalNumber = takeInnerText(semiTotal);
-
-//         let newTotalBalance = totalBalanceNumber - inputNumber;
-//         let newSemiTotal = semiTotalNumber + inputNumber;
-
-//         if(newTotalBalance < 0) {
-//             alert("Insufficient Balance!!!")
-//             return;
-//         }      
-//          document.getElementById(finalTotal).innerText = newTotalBalance;
-//          document.getElementById(semiTotal).innerText = newSemiTotal;
-//          document.getElementById(inputID).value = ''; 
-
-//          my_modal_5.showModal();   
-
-
-//     }   
-// }
+handleDonation('donateNowOne', 'inputOne', 'totalBalance', 'firstTotal');
+handleDonation('donateNowTwo', 'inputTwo', 'totalBalance', 'secondTotal');
+handleDonation('donateNowThree', 'inputThree', 'totalBalance', 'thirdTotal');
